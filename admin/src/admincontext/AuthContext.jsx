d5253ea21781e0af15956cded33a71b80bd514bd
Arun Kumar Prasad <arun.prashad02@gmail.com>
2025-07-31 11:08:41 +0530

    Update AuthContext.jsx

--- admin/src/admincontext/AuthContext.jsx
+++ admin/src/admincontext/AuthContext.jsx
@@ -2,7 +2,7 @@ import React , { createContext} from 'react'
 export const authDataContext = createContext()
 
 function authContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = "https://smartcart-backend1.onrender.com"
     let value = {
          serverUrl
     }
