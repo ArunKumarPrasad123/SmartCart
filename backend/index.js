3f5f0ebb8a8e2cc0607260e897b1fc30adede5ca
Arun Kumar Prasad <arun.prashad02@gmail.com>
2025-07-31 11:01:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["https://smartcart-frontendone.onrender.com", "http://localhost:5174"],
+  origin: ["https://smartcart-frontendone.onrender.com", "https://smartcart-admin.onrender.com"],
   credentials: true
 }))
 
